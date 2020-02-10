7de4f242b9a9b97e37b1dd4f9c4b6d6dc0817c81
Raghav Sharma <raghavs@dropbox.com>
2020-02-10 13:27:53 -0800

    [Scorecard] Shard current map

--- scorecard/scorecard_impl.go
+++ scorecard/scorecard_impl.go
@@ -17,10 +17,13 @@ limitations under the License.
 package scorecard
 
 import (
+	"hash/fnv"
 	"sync"
 )
 
 // Implementation of the interfaces in scorecard.go.
+
+const numBuckets = 16 // Picked arbitrarily. Ideally should be tuned.
 
 type scorecardImpl struct {
 	// rulesMu protects rules and ctg.
@@ -28,15 +31,14 @@ type scorecardImpl struct {
 	// contention becomes a problem, therefore RWMutex.
 	rulesMu sync.RWMutex
 	rules   []Rule
-	ctg     *compoundTagGenerator
+	ctg     *CompoundTagGenerator
 
-	// separate mutex for current to avoid contention with reading pointers above
-	// for each rule.
-	currentMu sync.Mutex
-	current   map[Tag]uint
+	// To reduce lock contention, every `Tag` is mapped to one of `numBuckets` buckets which has
+	// its own lock.
+	tagScoresBuckets [numBuckets]*tagScores
 }
 
-func getRulesAndTagGenerator(rules []Rule) ([]Rule, *compoundTagGenerator) {
+func getRulesAndTagGenerator(rules []Rule) ([]Rule, *CompoundTagGenerator) {
 	// Dedup the rules
 	dedupedRules := make([]Rule, 0, len(rules))
 	ruleMap := make(map[string]struct{}, len(rules))
@@ -48,15 +50,19 @@ func getRulesAndTagGenerator(rules []Rule) ([]Rule, *compoundTagGenerator) {
 		}
 	}
 
-	return dedupedRules, newCompoundTagGenerator(dedupedRules)
+	return dedupedRules, NewCompoundTagGenerator(dedupedRules)
 }
 
 func newScorecard(rules []Rule) Scorecard {
 	rules, ctg := getRulesAndTagGenerator(rules)
+	var tagScoresBuckets [numBuckets]*tagScores
+	for i := range tagScoresBuckets {
+		tagScoresBuckets[i] = &tagScores{tagToScore: make(map[Tag]uint)}
+	}
 	return &scorecardImpl{
 		rules:   rules,
 		ctg:     ctg,
-		current: make(map[Tag]uint),
+		tagScoresBuckets: tagScoresBuckets,
 	}
 }
 
@@ -83,32 +89,20 @@ func ruleFor(rules []Rule, tag Tag) Rule {
 
 func (s *scorecardImpl) TrackRequest(tags []Tag) *TrackingInfo {
 	s.rulesMu.RLock()
+	rules := s.rules
 	ctg := s.ctg
 	s.rulesMu.RUnlock()
-
-	// NOTE(opaugam) - perform the cartesian product of tag X non-atomic rules. The
-	// shortcuts array holds a back pointer to the corresponding rule for each output tag.
-	var rule Rule
-	shortcuts, expanded := ctg.combine(tags)
-	expanded = append(expanded, tags...)
-	for idx, tag := range expanded {
-		// NOTE(opaugam) - the first n tags can by definition be mapped to their rule
-		// without globbing. We still need to enforce a linear scan+glob for the rest
-		// (e.g the input tags) but do it only on whatever atomic rules we have. If we
-		// can't match we'll default to a Rule{} placeholder.
-		if idx < len(shortcuts) {
-			rule = shortcuts[idx].rule
-		} else {
-			rule = ruleFor(ctg.singleFragmentRules, tag)
-		}
-
+	allTags := ctg.Generate(tags)
+	allTags = append(allTags, tags...)
+	for idx, tag := range allTags {
+		rule := ruleFor(rules, tag)
 		if s.shouldIsolateTag(tag, rule) {
 			// shouldIsolate tracks rules in the scorecard. For now, it only tracks when the rule
 			// isn't violated (i.e. doesn't hit this branch). That means we will want to untrack the
-			// previous idx rules, so we take expanded[:idx] (end range is EXCLUSIVE). If
+			// previous idx rules, so we take allTags[:idx] (end range is EXCLUSIVE). If
 			// shouldIsolate changes to add in either case, then we should make sure to unset
-			// expanded[idx] as well.
-			s.rawUntrackRequest(expanded[:idx])
+			// allTags[idx] as well.
+			s.rawUntrackRequest(allTags[:idx])
 			return &TrackingInfo{
 				Tracked:  false,
 				Violated: rule,
@@ -119,7 +113,7 @@ func (s *scorecardImpl) TrackRequest(tags []Tag) *TrackingInfo {
 	}
 	return &TrackingInfo{
 		Tracked:     true,
-		trackedTags: expanded,
+		trackedTags: allTags,
 		scorecard:   s,
 		callback:    rawUntrackCallback,
 	}
@@ -143,54 +137,67 @@ func (s *scorecardImpl) rawUntrackRequest(tags []Tag) {
 	}
 }
 
-// Perform locking in these isolated functions.  Touching the map elsewhere is
-// discouraged in order to isolate the concurrency.
-//
-// For this lock/map design, it will have lower throughput, but also lower
-// latency variance assuming the mutex can be fair (latency will grow uniformly
-// across the requests).
-//
-// For a lockfree map design, or an otherwise scalable map, this should have
-// both higher throughput and lower latency.
 func (s *scorecardImpl) shouldIsolateTag(tag Tag, rule Rule) bool {
-	s.currentMu.Lock()
-	defer s.currentMu.Unlock()
-	current := s.currentScore(tag)
-	isolate := !rule.isDefaultValue() && current >= rule.Capacity
-	// overflow check
-	if current+1 < current {
-		isolate = true
-	}
-	if !isolate {
-		s.current[tag] = current + 1
-	}
-	return isolate
+	return s.bucket(tag).shouldIsolateTag(tag, rule)
 }
 
 func (s *scorecardImpl) removeReference(tag Tag) {
-	s.currentMu.Lock()
-	defer s.currentMu.Unlock()
-	current := s.currentScore(tag)
-	if current > 1 {
-		s.current[tag] = current - 1
-	} else if current > 0 {
-		delete(s.current, tag)
-	}
+	s.bucket(tag).removeReference(tag)
 }
 
-func (s *scorecardImpl) currentScore(tag Tag) uint {
-	if x, ok := s.current[tag]; ok {
-		return x
-	}
-	return 0
+func (s *scorecardImpl) bucket(tag Tag) *tagScores {
+	// This hash was picked arbitrarily. The fastest, crudest, hash is probably ideal.
+	h := fnv.New32a()
+	h.Write([]byte(tag))
+	return s.tagScoresBuckets[h.Sum32() % numBuckets]
 }
 
 func (s *scorecardImpl) Inspect() map[Tag]uint {
 	ret := make(map[Tag]uint)
-	s.currentMu.Lock()
-	defer s.currentMu.Unlock()
-	for t, v := range s.current {
-		ret[t] = v
+	for _, bucket := range s.tagScoresBuckets {
+		bucket.lock.Lock()
+		for t, v := range bucket.tagToScore {
+			ret[t] = v
+		}
+		bucket.lock.Unlock()
 	}
 	return ret
+}
+
+type tagScores struct {
+	lock sync.Mutex
+	tagToScore   map[Tag]uint
+}
+
+func (t *tagScores) shouldIsolateTag(tag Tag, rule Rule) bool {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	score := t.score(tag)
+	isolate := !rule.isDefaultValue() && score >= rule.Capacity
+	// overflow check
+	if score+1 < score {
+		isolate = true
+	}
+	if !isolate {
+		t.tagToScore[tag] = score + 1
+	}
+	return isolate
+}
+
+func (t *tagScores) removeReference(tag Tag) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	score := t.score(tag)
+	if score > 1 {
+		t.tagToScore[tag] = score - 1
+	} else if score > 0 {
+		delete(t.tagToScore, tag)
+	}
+}
+
+func (t *tagScores) score(tag Tag) uint {
+	if x, ok := t.tagToScore[tag]; ok {
+		return x
+	}
+	return 0
 }
